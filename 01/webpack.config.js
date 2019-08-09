1c5350eec02f4d21e2b6b45e311f6e95ee4a438c
邱福利 <qiufuli@jsb5.com>
2019-08-09 17:54:57 +0800

    '080901'

---
+++ 01/webpack.config.js
@@ -0,0 +1,8 @@
+//webpack 是node写出来的 采用node的写法
+// node 采用的是commonJS的写法 require(xxx)  module.exports xxxx
+
+//webpack2.x 以上就开始兼容 es module的写法 import  resport default
+
+module.exports = {
+  entry:'./src/index.js' //入口
+}
